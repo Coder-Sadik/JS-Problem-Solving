1a23aaeaaa87e864eaf5d884407467f53a3ec905
Sadik Rahman <srahman2702@gmail.com>
2024-10-23 01:18:51 +0600

    added solution for parseString

---
+++ Solutions/parseString.js
@@ -0,0 +1,31 @@
+function parseString(...inputs) {
+	let sum = 0;
+	let text = "";
+
+	for (let input of inputs) {
+		if (!isNaN(input)) {
+			sum += Number(input);
+		} else {
+			return inputs.join(" ");
+		}
+	}
+
+	return sum;
+}
+
+// Sample Inputs and Outputs
+console.log(parseString("21", "24", "40"));
+console.log(parseString("Hello", "Alpha"));
+console.log(parseString("Summer", "2022"));
+
+//problem :
+// Write a Javascript Program that takes String as a parameter and checks if the parameters are number or text. If the parameters are numbers then it will return a summation of the numbers. If the parameters are not numbers then it will generate a text by concatenating the strings.
+// Sample Input:
+// parseString(“21” , “24’ , “40”)
+// parseString(“Hello” , “Alpha”)
+// parseString(“Summer” , “2022”)
+
+// Sample Output:
+// 85
+// Hello Alpha
+// Summer 2022
---
+++ tempCodeRunnerFile.javascript
@@ -0,0 +1,7 @@
+
+let x = "  35   ";
+let y = "  35 ";
+console.log(!isNaN(x));
+console.log(Number(x) + Number(y));
+
+
